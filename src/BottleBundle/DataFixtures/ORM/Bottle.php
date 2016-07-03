9c6f87358764650afa6f7267799dab757b33893a
Fantin VERPILLOT <fantin.verpillot@epita.fr>
2016-07-03 21:19:12 +0200

    push bottle fixtures

---
+++ src/BottleBundle/DataFixtures/ORM/Bottle.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace BottleBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use BottleBundle\Entity\Bottle;
+use \Faker\Factory as Faker;
+
+class LoadBottleData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker::create();
+        $bottles = array(
+            array(
+                'fkReceiver'        => null,
+                'fkTransmitter'     => $this->getReference('User0'),
+                'state'             => 0,
+                'mark'              => null,
+                'fkEmoji'           => null,
+                'latitude'          => null,
+                'longitude'         => null,
+                'message'           => $faker->realText(1000),
+                'image'             => null,
+            ),
+            array(
+                'fkReceiver'        => null,
+                'fkTransmitter'     => $this->getReference('User0'),
+                'state'             => 1,
+                'mark'              => null,
+                'fkEmoji'           => null,
+                'latitude'          => null,
+                'longitude'         => null,
+                'message'           => $faker->realText(1000),
+                'image'             => null,
+            ),
+            array(
+                'fkReceiver'        => null,
+                'fkTransmitter'     => $this->getReference('User0'),
+                'state'             => 1,
+                'mark'              => null,
+                'fkEmoji'           => null,
+                'latitude'          => null,
+                'longitude'         => null,
+                'message'           => $faker->realText(1000),
+                'image'             => 'http://okux.org/wp-content/uploads/2013/07/fond-d-ecran-coucher-de-soleil-en-taille-reelle.jpg',
+            ),
+            array(
+                'fkReceiver'        => $this->getReference('User1'),
+                'fkTransmitter'     => $this->getReference('User0'),
+                'state'             => 2,
+                'mark'              => 3,
+                'fkEmoji'           => $this->getReference('Emoji0'),
+                'latitude'          => 48.856614,
+                'longitude'         => 2.3522219000000177,
+                'message'           => $faker->realText(1000),
+                'image'             => null,
+            ),
+            array(
+                'fkReceiver'        => $this->getReference('User1'),
+                'fkTransmitter'     => $this->getReference('User2'),
+                'state'             => 3,
+                'mark'              => 1,
+                'fkEmoji'           => $this->getReference('Emoji1'),
+                'latitude'          => 48.856614,
+                'longitude'         => 2.3522219000000177,
+                'message'           => $faker->realText(1000),
+                'image'             => 'http://img1.mxstatic.com/wallpapers/9c3d9ad540db9c53767569ae2faa15ac_large.jpeg',
+            ),
+        );
+
+        foreach ($bottles as $key => $value) {
+            $bottle = new Bottle();
+            $bottle->setMessage($value['fkReceiver']);
+            $bottle->setMessage($value['fkTransmitter']);
+            $bottle->setMessage($value['state']);
+            $bottle->setMessage($value['mark']);
+            $bottle->setMessage($value['fkEmoji']);
+            $bottle->setMessage($value['latitude']);
+            $bottle->setMessage($value['longitude']);
+            $bottle->setMessage($value['message']);
+            $bottle->setMessage($value['image']);
+
+            $manager->persist($bottle);
+            $this->addReference('Bottle'.$key, $bottle);
+        }
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 4;
+    }
+}
