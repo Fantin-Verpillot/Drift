e9ef739f304b587b178ca33fa2e373c353131d29
Fantin VERPILLOT <fantin.verpillot@epita.fr>
2016-07-03 20:49:06 +0200

    faker and fixtures installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new BottleBundle\BottleBundle(),
             new MainBundle\MainBundle(),
             new ReportBundle\ReportBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
