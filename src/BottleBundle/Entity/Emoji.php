1410aa64d186ed26eca2c361714f1e84b432a898
Fantin VERPILLOT <fantin.verpillot@epita.fr>
2016-07-03 18:43:04 +0200

    create emoji

---
+++ src/BottleBundle/Entity/Emoji.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace BottleBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Emoji
+ *
+ * @ORM\Table(name="emoji")
+ * @ORM\Entity(repositoryClass="BottleBundle\Repository\EmojiRepository")
+ */
+class Emoji
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Emoji
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     * @return Emoji
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string 
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
