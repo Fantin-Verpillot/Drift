9b7e0026c216a6d7623a69f95cd6a2445b97ea29
MrLyfing <ilyfing@gmail.com>
2016-07-07 20:01:41 +0200

    add bottle admin test

---
+++ tests/BottleBundle/BottleAdminTest.php
@@ -0,0 +1,136 @@
+<?php
+namespace BottleBundle;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BottleAdmin extends WebTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+
+    public function testAvailableBottleAdminLuigi()
+    {
+        echo ") [BOTTLE] Test available bottles admin for Luigi... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userLuigi = $userRepository->findAll()[1];
+        $bottlesAvailableAdmin = $bottleAdminRepository->getAvailableBottleAdmin($userLuigi);
+        $this->assertNotEquals($bottlesAvailableAdmin, null);
+        echo "OK\n";
+    }
+
+    public function testAvailableBottleAdminMario()
+    {
+        echo ") [BOTTLE] Test available bottles admin for Mario... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userMario = $userRepository->findAll()[0];
+        $bottlesAvailableAdmin = $bottleAdminRepository->getAvailableBottleAdmin($userMario);
+        $this->assertEquals($bottlesAvailableAdmin, null);
+        echo "OK\n";
+    }
+
+    public function testNoAvailableBottleAdminForAdmin()
+    {
+        echo ") [BOTTLE] Test not available bottles admin for Admin... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userAdmin = $userRepository->findAll()[0];
+        $bottlesAvailableAdmin = $bottleAdminRepository->getAvailableBottleAdmin($userAdmin);
+        $this->assertEquals($bottlesAvailableAdmin, null);
+        echo "OK\n";
+    }
+
+
+    public function testPendingBottleAdminLuigi()
+    {
+        echo ") [BOTTLE] Test pending bottles admin for Luigi... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userLuigi = $userRepository->findAll()[1];
+        $bottlesPendingAdmin = $bottleAdminRepository->getPendingBottleAdmin($userLuigi);
+        $this->assertEquals($bottlesPendingAdmin, null);
+        echo "OK\n";
+    }
+
+    public function testPendingBottleAdminMario()
+    {
+        echo ") [BOTTLE] Test pending bottles admin for Mario... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userMario = $userRepository->findAll()[0];
+        $bottlesPendingAdmin = $bottleAdminRepository->getPendingBottleAdmin($userMario);
+        $this->assertEquals($bottlesPendingAdmin, null);
+        echo "OK\n";
+    }
+
+    public function testNoPendingBottleAdminForAdmin()
+    {
+        echo ") [BOTTLE] Test no pending bottles admin for Admin... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userAdmin = $userRepository->findAll()[2];
+        $bottlesPendingAdmin = $bottleAdminRepository->getPendingBottleAdmin($userAdmin);
+        $this->assertEquals($bottlesPendingAdmin, null);
+        echo "OK\n";
+    }
+
+    public function testSavedBottlesAdminLuigi()
+    {
+        echo ") [BOTTLE] Test saved bottles admin for Luigi... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userLuigi = $userRepository->findAll()[1];
+        $bottlesSavedAdmin = $bottleAdminRepository->getSavedAdminBottles($userLuigi);
+        $this->assertNotEquals($bottlesSavedAdmin, null);
+        echo "OK\n";
+    }
+
+    public function testSavedBottlesAdminMario()
+    {
+        echo ") [BOTTLE] Test saved bottles admin for Mario... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userMario = $userRepository->findAll()[0];
+        $bottlesSavedAdmin = $bottleAdminRepository->getSavedAdminBottles($userMario);
+        $this->assertNotEquals($bottlesSavedAdmin, null);
+        echo "OK\n";
+    }
+
+    public function testNoSavedBottlesAdminForAdmin()
+    {
+        echo ") [BOTTLE] Test no saved bottles admin for Admin... ";
+        $userRepository = $this->em->getRepository('MainBundle:User');
+        $bottleAdminRepository = $this->em->getRepository('BottleBundle:BottleAdmin');
+        $userAdmin = $userRepository->findAll()[2];
+        $bottlesSavedAdmin = $bottleAdminRepository->getSavedAdminBottles($userAdmin);
+        $this->assertNotEquals($bottlesSavedAdmin, null);
+        echo "OK\n";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
+}
